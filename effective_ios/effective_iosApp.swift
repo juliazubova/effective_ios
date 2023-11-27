ea5fbfa3dc0237af1610139715b90c5ea3b29783
Юлия Зубова <uzubova2020@gmail.com>
2023-11-27 10:16:38 +0600

    Initial Commit

---
+++ effective_ios/effective_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  effective_iosApp.swift
+//  effective_ios
+//
+//  Created by Effective Devices on 27.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct effective_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
